1140a0e6df91e4e5a4c8cf4515cb5e78383b1eb8
Naeemah1 <113605214+Naeemah1@users.noreply.github.com>
2024-05-12 05:20:36 +0100

    investigating the effect of varying epoch numbers

--- image-classification.py
+++ image-classification.py
@@ -19,14 +19,16 @@ model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 # Add Dense layers on top
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
+model.add(layers.Dense(11))
+#when change Received a label value of 9 which is outside the valid range of [0, 7).  Label values: 1 1 6 3 2 3 4 1 0 9 9 7 9 4 8 2 6 5 0 1 4 2 8 4 4 2 3 2 5 8 2 8
+#has to be int cant be float
 
 # Compile and train the model
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, 
+history = model.fit(train_images, train_labels, epochs=3, 
                     validation_data=(test_images, test_labels))
 
 # Evaluate the model
